85e5c8e495e67b86044fce66a9bdb099ac78477b
khd <khd>
2024-01-19 01:33:25 +0900

    샘플 수정

--- src/main/java/testing/demo/business/sample/repository/SampleMemberRepository.java
+++ src/main/java/testing/demo/business/sample/repository/SampleMemberRepository.java
@@ -1,5 +1,8 @@
 package testing.demo.business.sample.repository;
 
+import static testing.demo.business.sample.entity.QSampleMemberEntity.sampleMemberEntity;
+import static testing.demo.business.sample.entity.QSampleTeamEnity.sampleTeamEnity;
+
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -7,8 +10,8 @@ import org.springframework.stereotype.Repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import jakarta.persistence.EntityManager;
-import testing.demo.business.sample.entity.QSampleMemberEntity;
 import testing.demo.business.sample.entity.SampleMemberEntity;
+import testing.demo.business.sample.entity.SampleTeamEnity;
 
 @Repository
 public class SampleMemberRepository {
@@ -21,14 +24,23 @@ public class SampleMemberRepository {
     }
 
     public void test1(){
-        SampleMemberEntity sampleMemberEntity = new SampleMemberEntity();
-        sampleMemberEntity.setName("하이");
-        em.persist(sampleMemberEntity);
+        SampleMemberEntity sme = new SampleMemberEntity();
+        sme.setName("맴버1");
+        em.persist(sme);
 
-        QSampleMemberEntity a = QSampleMemberEntity.sampleMemberEntity;
-        List<SampleMemberEntity> result = queryFactory
-                                            .selectFrom(a)
+        SampleTeamEnity ste = new SampleTeamEnity();
+        ste.setName("팀1");
+        em.persist(ste);
+
+        List<SampleMemberEntity> resultMember = queryFactory
+                                                .selectFrom(sampleMemberEntity)
+                                                .fetch();
+
+        List<SampleTeamEnity> resultTeam = queryFactory
+                                            .selectFrom(sampleTeamEnity)
                                             .fetch();
-        System.out.println(result);
+
+        System.out.println(resultMember);
+        System.out.println(resultTeam);
     }
 }
